df470236a1be0426fd805d6bfc0de2c70290bbae
Pete Wilcox <petercwilcox@gmail.com>
2017-10-31 18:46:28 -0700

    beginning on fahrenheit conversion

---
+++ ex1-4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/* Print Fahrenheit-Celsius table for
+        fahr =0, 20, ..., 300 */
+
+int main()
+{
+    int fahr, celsius;
+    int lower, upper,  step;
+
+    lower = 0; /* lower limit of temperature table */
+    upper = 300; /* upper liimit of temperature table */
+    step = 20; /* step size */
+
+    fahr=lower;
+    while(fahr <= upper)
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
