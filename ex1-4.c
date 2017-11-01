770492182612b3c88e201ae6a3c5a843f7ea88fe
Pete Wilcox <petercwilcox@gmail.com>
2017-10-31 22:31:51 -0700

    book version

--- ex1-4.c
+++ ex1-4.c
@@ -8,37 +8,16 @@ int main()
     int fahr, celsius;
     int lower, upper,  step;
 
-    lower = 0; /* lower limit of temperature table */
-    upper = 300; /* upper liimit of temperature table */
-    step = 20; /* step size */
+    lower   = 0;      /* lower limit of temperature table */
+    upper   = 300;    /* upper liimit of temperature table */
+    step    = 20;  /* step size */
 
-    fahr=lower;
-    while(fahr <= upper)
+    fahr = lower;
+    while (fahr <= upper)
     {
-
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 }
