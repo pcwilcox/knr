ec37d119529b047329ff57042260ee0b4b4a9503
Pete Wilcox <petercwilcox@gmail.com>
2017-10-31 22:39:20 -0700

    That was actually 1.3

--- ex1-4.c
+++ ex1-4.c
@@ -5,18 +5,20 @@
 
 int main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper,  step;
 
-    lower   = 0;      /* lower limit of temperature table */
-    upper   = 300;    /* upper liimit of temperature table */
-    step    = 20;  /* step size */
+    lower   = 0;        /* lower limit of temperature table */
+    upper   = 300;      /* upper liimit of temperature table */
+    step    = 20;       /* step size */
 
+    printf("Fahrenheit\tCelsius\n");
+    printf("------------------------\n");
     fahr = lower;
     while (fahr <= upper)
     {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr - 32.0);
+        printf("%3.0f\t\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
     return 0;
