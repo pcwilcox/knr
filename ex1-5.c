e0ddbb0d1c99faca491a14f3b5b4f54bec9de4cd
Pete Wilcox <petercwilcox@gmail.com>
2017-11-02 10:26:19 -0700

    Now with #define

--- ex1-5.c
+++ ex1-5.c
@@ -2,12 +2,16 @@
 
 /* Print the Fahrenheit-Celsius table */
 
+#define     LOWER   0
+#define     UPPER   300
+#define     STEP    20
+
 int main()
 {
     int fahr;
     printf("Fahrenheit\tCelsius\n");
     printf("------------------------\n");
-    for (fahr = 300; fahr >=0; fahr -= 20)
+    for (fahr = UPPER; fahr >=LOWER; fahr -= STEP)
     {
         printf("%3d \t\t %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
     };
